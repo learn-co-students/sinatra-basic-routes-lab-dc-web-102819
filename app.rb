efa5fdde12b1692dad5c3e371ad8db0298e46466
remiodufuye <remi.odufuye@gmail.com>
2019-11-16 17:54:59 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+
+        get '/' do
+        "Hello, World!"
+        end
+
+
+        get '/name' do
+            "My name is __" 
+        end
+
+
+        get '/hometown' do
+            "My hometown is __"
+        end
+
+
+
+        get '/favorite-song' do
+            "My favorite song is __" 
+        end
+
+
+
+end 
